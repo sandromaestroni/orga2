cc5a9de640aba0f472402790dbba701db8ca8a01
Sandro <sandromaestroni06@gmail.com>
2022-12-03 10:51:40 -0300

    recu1 resuelto

--- resolucion/solucion/ej2/ej2.h
+++
@@ -1,9 +0,0 @@
-#ifndef EJ2_H_
-#define EJ2_H_
-
-#include<stdint.h>
-
-int16_t* filtro_c (const int16_t* entrada, unsigned size);
-int16_t* filtro (const int16_t* entrada, unsigned size);
-
-#endif //EJ2_H_
